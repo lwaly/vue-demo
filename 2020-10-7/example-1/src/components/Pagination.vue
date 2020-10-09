e96bf8cbe7410a45dc6ff83843a834114b4f8e99
lwaly <279527404@qq.com>
2020-10-09 17:46:34 +0800

    增加一个列表页

---
+++ 2020-10-7/example-1/src/components/Pagination.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="page">
+        <!-- <span class="demonstration">完整功能</span> -->
+        <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage4"
+            :page-sizes="[100, 200, 300, 400]"
+            :page-size="100"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400">
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Pagination',
+    components: {},
+    data() {
+        return {
+            currentPage4: 4,
+        };
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    destroyed() {},
+    methods: {
+        handleSizeChange(val) {
+            // eslint-disable-next-line no-console
+            console.log(`每页 ${val} 条`);
+        },
+        handleCurrentChange(val) {
+            // eslint-disable-next-line no-console
+            console.log(`当前页: ${val}`);
+        },
+    },
+};
+</script>
+<style lang='scss' scoped>
+.page {
+    width: 100%;
+    height: vh(30);
+    margin: 0 0 0  auto;
+    margin-top: vh(25);
+    line-height: vh(30);
+    text-align: right;
+}
+</style>
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vue-demo
 主要用于记录自己学习前端的实战历程
 第一第二阶段两个文件夹主要是学习https://github.com/lefex/FE项目的一些demo
+
+example-1是参照https://segmentfault.com/a/1190000012015667教程完成的。想学习的也可以参照这个实现一遍。
